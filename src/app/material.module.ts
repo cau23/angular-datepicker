14d10cfa871f2800e5484fc605e1f5e617fefc3e
Chad <chad.a.upchurch@gmail.com>
2018-09-18 22:58:42 -0500

    update-angular-material-setup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,10 @@
 // Custom Material Module File
+
 import { NgModule } from '@angular/core';
-import { MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatDatepickerModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatInputModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
